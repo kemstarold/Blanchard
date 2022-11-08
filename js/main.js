e6ebeb1ed06f85a2a00e5a153bb3d16de270fdb8
kemstarold <kemstar@rambler.ru>
2022-11-08 16:56:07 +0700

    add

--- js/main.js
+++ js/main.js
@@ -1,36 +1,52 @@
-let heroSwiper = new Swiper ('.hero-swiper', {
-  direction: 'horizontal',
-  // effect: 'cube',
-  // cubeEffect: {
-  //   slideShadows: true,
-  //   shadowOffset: 20,
-  //   shadowScale: 0.94
-  // },
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true
-  },
-  autoplay: {
-    delay: 3000,
-  },
-  speed: 2500,
-});
+window.addEventListener('DOMContentLoaded', function () {
+  let burger = document.querySelector('.header__burger');
+  let menu = document.querySelector('.header__top-menu');
 
-let galeiSwiper = new Swiper ('.galleri-swiper-container', {
-  direction: 'horizontal',
-  slidesPerView: 3,
-  slidesPerColumnFill: 'row',
-  slidesPerColumn: 2,
-  slidesPerGroup: 3,
-  spaceBetween: 50,
+  let heroSwiper = new Swiper('.hero-swiper', {
+    direction: 'horizontal',
+    // effect: 'cube',
+    // cubeEffect: {
+    //   slideShadows: true,
+    //   shadowOffset: 20,
+    //   shadowScale: 0.94
+    // },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    autoplay: {
+      delay: 3000,
+    },
+    speed: 2500,
+  });
 
-  pagination: {
-    el: '.galleri-button__pagination',
-    type: 'fraction',
-  },
+  let galeiSwiper = new Swiper('.galleri-swiper-container', {
+    direction: 'horizontal',
+    slidesPerView: 3,
+    slidesPerColumnFill: 'row',
+    slidesPerColumn: 2,
+    slidesPerGroup: 3,
+    spaceBetween: 50,
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    pagination: {
+      el: '.galleri-button__pagination',
+      type: 'fraction',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  burger.addEventListener('click',
+
+    function () {
+
+      burger.classList.toggle('burger-active');
+      menu.classList.toggle('nav-active');
+
+      document.body.classList.toggle('stop-scroll');
+    })
+
 })
